f42fa9da41f895d8e9362bfbe8435ac23c6e0e94
tarun42 <dastarun42@gmail.com>
2020-09-10 08:52:11 +0530

    added drawer

--- app/src/main/java/com/example/mvvmarchitecture/ui/home/HomeActivity.kt
+++ app/src/main/java/com/example/mvvmarchitecture/ui/home/HomeActivity.kt
@@ -2,11 +2,27 @@ package com.example.mvvmarchitecture.ui.home
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI
 import com.example.mvvmarchitecture.R
+import kotlinx.android.synthetic.main.activity_home.*
 
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+        setSupportActionBar(toolbar)
+
+        val navController =Navigation.findNavController(this,R.id.fragment)
+        NavigationUI.setupWithNavController(nav_view,navController)
+        NavigationUI.setupActionBarWithNavController(this,navController,drawer_layout)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return NavigationUI.navigateUp(
+            Navigation.findNavController(this,R.id.fragment),
+            drawer_layout
+        )
     }
 }
---
+++ app/src/main/java/com/example/mvvmarchitecture/ui/home/profile/ProfileViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.mvvmarchitecture.ui.home.profile
+
+import androidx.lifecycle.ViewModel
+
+class ProfileViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
